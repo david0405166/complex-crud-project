c119c52de214673e8a26ceb31346a176e4f46036
david0405166 <davidordonio92@gmail.com>
2024-06-16 01:54:41 +0800

    configure typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { User } from './users/user.entity'; 
+import { Post } from './posts/post.entity';
+import { PostModule } from './posts/post.module';
+
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type:'sqlite',
+    database:'db.sqlite',
+    entities:[User,Post],
+    synchronize:true
+}),AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
